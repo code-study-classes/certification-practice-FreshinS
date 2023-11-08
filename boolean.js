21db7d558b0c34866127d1fbea96c41115e48ace
FreshinS <freshinsboost@gmail.com>
2023-11-08 13:53:12 +0300

    loops + lint fixes

--- boolean.js
+++ boolean.js
@@ -8,19 +8,27 @@ export const checkInequality2 = (A, B) => (A >= 0) || (B < -2);
 
 export const checkBetweenNumbers = (A, B, C) => (B > A) && (B < C);
 
-export const checkOddThreeDigitNumber = (number) => (number % 2 === 1) && (number >= 100) && (number < 1000);
+export const checkOddThreeDigitNumber = (n) => (n % 2 === 1) && (n >= 100) && (n < 1000);
 
 export const checkUniqueDigits = (number) => {
-    const numberS = String(number);
-    let bool = true;
-    for (let i = 0; i < numberS.length - 1; i += 1) {
-        bool = (!(numberS.slice(i + 1).includes(numberS[i])) && bool);
-    }
-    return bool;
+  const numberS = String(number);
+  let bool = true;
+  for (let i = 0; i < numberS.length - 1; i += 1) {
+    bool = (!(numberS.slice(i + 1).includes(numberS[i])) && bool);
+  }
+  return bool;
 };
 
 export const isSecondQuadrant = (x, y) => (x < 0) && (y > 0);
 
-export const isIsoscelesTriangle = (a, b, c) => ((a == b) || (a == c) || (b == c)) && ((a !== 0) || (b !== 0) || (c !== 0));
+export const isIsoscelesTriangle = (a, b, c) => {
+  const cond1 = a === b || a === c || b === c;
+  const cond2 = a !== 0 || b !== 0 || c !== 0;
+  return cond1 && cond2;
+};
 
-export const areSameColorFields = (x1, y1, x2, y2) => ((x1 % 2 === y1 % 2) && (x2 % 2 === y2 % 2)) || ((x1 % 2 !== y1 % 2) && (x2 % 2 !== y2 % 2));
+export const areSameColorFields = (x1, y1, x2, y2) => {
+  const cond1 = (x1 % 2 === y1 % 2) && (x2 % 2 === y2 % 2);
+  const cond2 = (x1 % 2 !== y1 % 2) && (x2 % 2 !== y2 % 2);
+  return cond1 || cond2;
+};
--- numbers.js
+++ numbers.js
@@ -6,36 +6,42 @@ export const calculateKilobytes = (fileSizeInBytes) => Math.floor(fileSizeInByte
 
 export const calculateSegments = (lengthA, lengthB) => Math.floor(lengthA / lengthB);
 
-export const calculateDigitSum = (twoDigitNumber) => parseInt(String(twoDigitNumber)[0], 10) + parseInt(String(twoDigitNumber)[1], 10);
+export const calculateDigitSum = (twoDigitNumber) => {
+  const firstDigit = parseInt(String(twoDigitNumber)[0], 10);
+  const secondDigit = parseInt(String(twoDigitNumber)[1], 10);
+  return firstDigit + secondDigit;
+};
 
 export const swapHundredsAndTens = (threeDigitNumber) => parseInt(`${String(threeDigitNumber)[1]}${String(threeDigitNumber)[0]}${String(threeDigitNumber)[2]}`, 10);
 
 export const getHundredsDigit = (number) => Math.floor(number / 100) % 10;
 
 export const getFullHours = (seconds) => {
-    if (seconds !== Math.floor(seconds) || seconds < 0) {
-        throw 'error';
-    } 
-    return Math.floor(seconds / 3600);
-}
+  if (seconds !== Math.floor(seconds) || seconds < 0) {
+    throw TypeError;
+  }
+  return Math.floor(seconds / 3600);
+};
 
 export const getDayOfWeek = (dayOfYear) => {
-    switch (dayOfYear % 7) {
-        case 0:
-            return 0;
-        case 1:
-            return 1;
-        case 2:
-            return 2;
-        case 3:
-            return 3;
-        case 4:
-            return 4;
-        case 5:
-            return 5;
-        case 6:
-            return 6;
-    }
-}
+  switch (dayOfYear % 7) {
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    case 2:
+      return 2;
+    case 3:
+      return 3;
+    case 4:
+      return 4;
+    case 5:
+      return 5;
+    case 6:
+      return 6;
+    default:
+      return 0;
+  }
+};
 
 export const countSquares = (A, B, C) => Math.floor(A / C) * Math.floor(B / C);
